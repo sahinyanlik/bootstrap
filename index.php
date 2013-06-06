9a0c6bea40fc6c55005607179bc981399dc6a5c1
sahin <sahinyanlik@gmail.com>
2013-06-06 09:19:50 +0300

    test

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: sahin
+ * Date: 06.06.2013
+ * Time: 09:16
+ * To change this template use File | Settings | File Templates.
+ */
+echo phpinfo();
